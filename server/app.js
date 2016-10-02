e22e88a6a23e0a0d58ad860f80b5910b1651a0be
Harrison Lingren <harrisonlingren@gmail.com>
2016-10-01 22:50:50 -0400

    switch to mongoose and setup create endpoint

--- server/app.js
+++ server/app.js
@@ -5,7 +5,16 @@ var app = express();
 app.use(bp.json());
 app.use(bp.urlencoded({extended: true}));
 
-var routes = require('./routes/routes.js')(app);
-var server = app.listen(3000, function() {
-  console.log('Listening on port %s...', server.address().port);
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://myfood:myfood@ds033337.mongolab.com:33337/myfood');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  var models = require('./models/models.js')(mongoose);
+  var routes = require('./routes/routes.js')(app, mongoose, models);
+  var server = app.listen(3000, function() {
+    console.log('Listening on port %s...', server.address().port);
+  });
 });
