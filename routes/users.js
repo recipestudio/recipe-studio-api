834fd0a5efe17dd18d6f8f657993ecd87286aefe
Harrison Lingren <harrisonlingren@gmail.com>
2017-12-17 03:03:55 -0500

    config file => envvar

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,10 @@ var router = express.Router();
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../firebaseadmin.json");
+let serviceAccount = {
+  private_key: (process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
